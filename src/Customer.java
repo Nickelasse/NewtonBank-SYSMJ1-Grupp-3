fb8cf40a67e4e922feb39f0d95e925a212101d51
LidiyaD <lidiya_darafshi@hotmail.com>
2015-11-09 12:07:43 +0100

    Customer class

---
+++ src/Customer.java
@@ -0,0 +1,42 @@
+
+import java.util.ArrayList;
+
+public class Customer {
+
+	private String name;
+	private long pNr;
+	private ArrayList<SavingsAccount> accounts;
+
+	private Customer(String name, long pNr) {
+		this.name = name;
+		this.pNr = pNr;
+		this.accounts = new ArrayList<>();
+
+	}
+
+	// Get Name
+	public String getName() {
+		return name;
+
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// Get PersonNummer
+	public long getpNr() {
+		return pNr;
+	}
+
+	public ArrayList<SavingsAccount> getAccounts() {
+		return accounts;
+	}
+
+	// To String - Message
+	public String toString() {
+		String str = "Namn: " + name + "\n" + "Personnummer: (" + pNr + ")";
+		return str;
+	}
+
+}
